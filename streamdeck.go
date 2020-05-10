09fa1a3d2b8706ee9c028c010d6554df0fc34ae5
Florian Thienel <florian@thienel.org>
2020-05-10 15:30:43 +0200

    reduce duplication by using functions for specific behavior instead of separate types for each hardware variant

--- streamdeck.go
+++ streamdeck.go
@@ -1,9 +1,11 @@
 package streamdeck
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/color"
+	"image/jpeg"
 
 	"github.com/karalabe/hid"
 	"golang.org/x/image/draw"
@@ -17,6 +19,16 @@ const (
 	PID_STREAMDECK_XL   = 0x006c
 )
 
+var (
+	c_DEFAULT_FIRMWARE   = []byte{0x04}
+	c_DEFAULT_RESET      = []byte{0x0b, 0x63}
+	c_DEFAULT_BRIGHTNESS = []byte{0x05, 0x55, 0xaa, 0xd1, 0x01}
+
+	c_XL_FIRMWARE   = []byte{0x05}
+	c_XL_RESET      = []byte{0x03, 0x02}
+	c_XL_BRIGHTNESS = []byte{0x03, 0x08}
+)
+
 // Device represents a single Stream Deck device
 type Device struct {
 	ID     string
@@ -26,13 +38,22 @@ type Device struct {
 	Rows    uint8
 	Pixels  uint
 
-	state []byte
+	featureReportSize   int
+	firmwareOffset      int
+	keyStateOffset      int
+	imagePageSize       int
+	imagePageHeaderSize int
+	toImageFormat       func(image.Image) ([]byte, error)
+	imagePageHeader     func(pageIndex int, keyIndex uint8, payloadLength int, lastPage bool) []byte
+
+	getFirmwareCommand   []byte
+	resetCommand         []byte
+	setBrightnessCommand []byte
+
+	keyState []byte
 
 	device *hid.Device
 	info   hid.DeviceInfo
-
-	newHardware func(device *hid.Device) hardware
-	hardware    hardware
 }
 
 // Key holds the current status of a key on the device
@@ -47,64 +68,81 @@ func Devices() ([]Device, error) {
 
 	devs := hid.Enumerate(VID_ELGATO, 0)
 	for _, d := range devs {
-		if d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK {
-			dev := Device{
-				ID:          d.Path,
-				Serial:      d.Serial,
-				Columns:     5,
-				Rows:        3,
-				Pixels:      72,
-				state:       make([]byte, 5*3), // Columns * Rows
-				info:        d,
-				newHardware: newClassicHardware,
+		var dev Device
+
+		switch {
+		case d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK:
+			dev = Device{
+				ID:                   d.Path,
+				Serial:               d.Serial,
+				Columns:              5,
+				Rows:                 3,
+				Pixels:               72,
+				featureReportSize:    17,
+				firmwareOffset:       5,
+				keyStateOffset:       1,
+				imagePageSize:        8192,
+				imagePageHeaderSize:  16,
+				imagePageHeader:      defaultImagePageHeader,
+				toImageFormat:        toBMP,
+				getFirmwareCommand:   c_DEFAULT_FIRMWARE,
+				resetCommand:         c_DEFAULT_RESET,
+				setBrightnessCommand: c_DEFAULT_BRIGHTNESS,
 			}
-
-			dd = append(dd, dev)
-		}
-		if d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_MINI {
-			dev := Device{
-				ID:          d.Path,
-				Serial:      d.Serial,
-				Columns:     3,
-				Rows:        2,
-				Pixels:      80,
-				state:       make([]byte, 3*2), // Columns * Rows
-				info:        d,
-				newHardware: newMiniHardware,
+		case d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_MINI:
+			dev = Device{
+				ID:                   d.Path,
+				Serial:               d.Serial,
+				Columns:              3,
+				Rows:                 2,
+				Pixels:               80,
+				featureReportSize:    17,
+				firmwareOffset:       5,
+				keyStateOffset:       1,
+				imagePageSize:        1024,
+				imagePageHeaderSize:  16,
+				imagePageHeader:      defaultImagePageHeader,
+				toImageFormat:        toBMP,
+				getFirmwareCommand:   c_DEFAULT_FIRMWARE,
+				resetCommand:         c_DEFAULT_RESET,
+				setBrightnessCommand: c_DEFAULT_BRIGHTNESS,
 			}
-
-			dd = append(dd, dev)
-		}
-
-		/*
-			if d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_V2 {
-				dev := Device{
-					ID:         d.Path,
-					Serial:     d.Serial,
-					Columns:    5,
-					Rows:       3,
-					Pixels:     72,
-					state:      make([]byte, 5*3), // Columns * Rows
-					info:       d,
-					newHardware: newXLHardware,
+		case d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_V2:
+			/*
+					dev = Device{
+						ID:         d.Path,
+						Serial:     d.Serial,
+						Columns:    5,
+						Rows:       3,
+						Pixels:     72,
+						state:      make([]byte, 5*3), // Columns * Rows
+					}
 				}
-
-				dd = append(dd, dev)
+			*/
+		case d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_XL:
+			dev = Device{
+				ID:                   d.Path,
+				Serial:               d.Serial,
+				Columns:              8,
+				Rows:                 4,
+				Pixels:               96,
+				featureReportSize:    32,
+				firmwareOffset:       6,
+				keyStateOffset:       4,
+				imagePageSize:        1024,
+				imagePageHeaderSize:  8,
+				imagePageHeader:      xlImagePageHeader,
+				toImageFormat:        toJPEG,
+				getFirmwareCommand:   c_XL_FIRMWARE,
+				resetCommand:         c_XL_RESET,
+				setBrightnessCommand: c_XL_BRIGHTNESS,
 			}
-		*/
 
-		if d.VendorID == VID_ELGATO && d.ProductID == PID_STREAMDECK_XL {
-			dev := Device{
-				ID:          d.Path,
-				Serial:      d.Serial,
-				Columns:     8,
-				Rows:        4,
-				Pixels:      96,
-				state:       make([]byte, 8*4), // Columns * Rows
-				info:        d,
-				newHardware: newXLHardware,
-			}
+		}
 
+		if dev.ID != "" {
+			dev.keyState = make([]byte, dev.Columns*dev.Rows)
+			dev.info = d
 			dd = append(dd, dev)
 		}
 	}
@@ -120,7 +158,6 @@ func (d *Device) Open() error {
 	if err != nil {
 		return err
 	}
-	d.hardware = d.newHardware(d.device)
 	return nil
 }
 
@@ -131,12 +168,16 @@ func (d Device) Close() error {
 
 // FirmwareVersion returns the firmware version of the device
 func (d Device) FirmwareVersion() (string, error) {
-	return d.hardware.FirmwareVersion()
+	result, err := d.getFeatureReport(d.getFirmwareCommand)
+	if err != nil {
+		return "", err
+	}
+	return string(result[d.firmwareOffset:]), nil
 }
 
 // Resets the Stream Deck, clears all button images and shows the standby image
 func (d Device) Reset() error {
-	return d.hardware.Reset()
+	return d.sendFeatureReport(d.resetCommand)
 }
 
 // Clears the Stream Deck, setting a black image on all buttons
@@ -157,22 +198,23 @@ func (d Device) Clear() error {
 // ReadKeys returns a channel, which it will use to emit key presses/releases
 func (d Device) ReadKeys() (chan Key, error) {
 	kch := make(chan Key)
-	b := make([]byte, len(d.state))
+	keyBuffer := make([]byte, d.keyStateOffset+len(d.keyState))
 	go func() {
 		for {
-			copy(d.state, b)
+			copy(d.keyState, keyBuffer[d.keyStateOffset:])
 
-			err := d.hardware.ReadKeyState(b)
+			_, err := d.device.Read(keyBuffer)
 			if err != nil {
 				close(kch)
 				return
 			}
 
-			for i := 0; i < len(b); i++ {
-				if b[i] != d.state[i] {
+			for i := d.keyStateOffset; i < len(keyBuffer); i++ {
+				keyIndex := uint8(i - d.keyStateOffset)
+				if keyBuffer[i] != d.keyState[keyIndex] {
 					kch <- Key{
-						Index:   d.translateKeyIndex(uint8(i)),
-						Pressed: b[i] == 1,
+						Index:   d.translateKeyIndex(keyIndex),
+						Pressed: keyBuffer[i] == 1,
 					}
 				}
 			}
@@ -187,7 +229,12 @@ func (d Device) SetBrightness(percent uint8) error {
 	if percent > 100 {
 		percent = 100
 	}
-	return d.hardware.SetBrightness(percent)
+
+	report := make([]byte, len(d.setBrightnessCommand)+1)
+	copy(report, d.setBrightnessCommand)
+	report[len(report)-1] = percent
+
+	return d.sendFeatureReport(report)
 }
 
 // SetImage sets the image of a button on the Stream Deck. The provided image
@@ -199,19 +246,23 @@ func (d Device) SetImage(index uint8, img image.Image) error {
 		return fmt.Errorf("supplied image has wrong dimensions, expected %[1]dx%[1]d pixels", d.Pixels)
 	}
 
-	imageData, err := d.hardware.GetImageData(img)
+	imageBytes, err := d.toImageFormat(img)
 	if err != nil {
-		return fmt.Errorf("cannot get image data: %v", err)
+		return fmt.Errorf("cannot convert image data: %v", err)
+	}
+	imageData := imageData{
+		image:    imageBytes,
+		pageSize: d.imagePageSize - d.imagePageHeaderSize,
 	}
 
-	data := make([]byte, d.hardware.ImagePageSize())
+	data := make([]byte, d.imagePageSize)
 
 	var page int
 	var lastPage bool
 	for !lastPage {
 		var payload []byte
 		payload, lastPage = imageData.Page(page)
-		header := d.hardware.GetImagePageHeader(page, d.translateKeyIndex(index), len(payload), lastPage)
+		header := d.imagePageHeader(page, d.translateKeyIndex(index), len(payload), lastPage)
 
 		copy(data, header)
 		copy(data[len(header):], payload)
@@ -232,6 +283,25 @@ func (d Device) translateKeyIndex(index uint8) uint8 {
 	return (index - keyCol) + (d.Columns - 1) - keyCol
 }
 
+// getFeatureReport from the device without worries about the correct payload size.
+func (d Device) getFeatureReport(payload []byte) ([]byte, error) {
+	b := make([]byte, d.featureReportSize)
+	copy(b, payload)
+	_, err := d.device.GetFeatureReport(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// sendFeatureReport to the device without worries about the correct payload size.
+func (d Device) sendFeatureReport(payload []byte) error {
+	b := make([]byte, d.featureReportSize)
+	copy(b, payload)
+	_, err := d.device.SendFeatureReport(b)
+	return err
+}
+
 // toRGBA converts an image.Image to an image.RGBA
 func toRGBA(img image.Image) *image.RGBA {
 	switch img.(type) {
@@ -241,4 +311,135 @@ func toRGBA(img image.Image) *image.RGBA {
 	out := image.NewRGBA(img.Bounds())
 	draw.Copy(out, image.Pt(0, 0), img, img.Bounds(), draw.Src, nil)
 	return out
+}
+
+// toBMP returns the raw bytes of the given image in BMP format, flipped horizontally
+func toBMP(img image.Image) ([]byte, error) {
+	rgba := toRGBA(img)
+
+	// this is a BMP file header followed by a BPM bitmap info header
+	// find more information here: https://en.wikipedia.org/wiki/BMP_file_format
+	header := []byte{
+		0x42, 0x4d, 0xf6, 0x3c, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x36, 0x00, 0x00, 0x00, 0x28, 0x00,
+		0x00, 0x00, 0x48, 0x00, 0x00, 0x00, 0x48, 0x00,
+		0x00, 0x00, 0x01, 0x00, 0x18, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0xc0, 0x3c, 0x00, 0x00, 0xc4, 0x0e,
+		0x00, 0x00, 0xc4, 0x0e, 0x00, 0x00, 0x00, 0x00,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+
+	buffer := make([]byte, len(header)+rgba.Bounds().Dx()*rgba.Bounds().Dy()*3)
+	copy(buffer, header)
+
+	i := len(header)
+	for y := 0; y < rgba.Bounds().Dy(); y++ {
+		// flip image horizontally
+		for x := rgba.Bounds().Dx() - 1; x >= 0; x-- {
+			c := rgba.RGBAAt(x, y)
+			buffer[i] = c.B
+			buffer[i+1] = c.G
+			buffer[i+2] = c.R
+			i += 3
+		}
+	}
+	return buffer, nil
+}
+
+// toJPEG returns the raw bytes of the given image in JPEG format, flipped horizontally and vertically.
+func toJPEG(img image.Image) ([]byte, error) {
+	// flip image horizontally and vertically
+	flipped := image.NewRGBA(img.Bounds())
+	draw.Copy(flipped, image.ZP, img, img.Bounds(), draw.Src, nil)
+	for y := 0; y < flipped.Bounds().Dy()/2; y++ {
+		yy := flipped.Bounds().Max.Y - y - 1
+		for x := 0; x < flipped.Bounds().Dx(); x++ {
+			xx := flipped.Bounds().Max.X - x - 1
+
+			c := flipped.RGBAAt(x, y)
+			flipped.SetRGBA(x, y, flipped.RGBAAt(xx, yy))
+			flipped.SetRGBA(xx, yy, c)
+		}
+	}
+
+	buffer := bytes.NewBuffer([]byte{})
+	err := jpeg.Encode(buffer, flipped, nil)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), err
+}
+
+// defaultImagePageHeader returns the image page header sequence used by the Stream Deck and Stream Deck Mini.
+func defaultImagePageHeader(pageIndex int, keyIndex uint8, payloadLength int, lastPage bool) []byte {
+	var lastPageByte byte
+	if lastPage {
+		lastPageByte = 0
+	}
+	return []byte{
+		0x02, 0x01,
+		byte(pageIndex + 1), 0x00,
+		lastPageByte,
+		keyIndex + 1,
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+	}
+}
+
+// xlImagePageHeader returns the image page header sequence used by the Stream Deck XL.
+func xlImagePageHeader(pageIndex int, keyIndex uint8, payloadLength int, lastPage bool) []byte {
+	var lastPageByte byte
+	if lastPage {
+		lastPageByte = 1
+	}
+	return []byte{
+		0x02, 0x07, keyIndex, lastPageByte,
+		byte(payloadLength), byte(payloadLength >> 8),
+		byte(pageIndex), byte(pageIndex >> 8),
+	}
+}
+
+// imageData allows to access raw image data in a byte array through pages of a given size.
+type imageData struct {
+	image    []byte
+	pageSize int
+}
+
+// Page returns the page with the given index and an indication if this is the last page.
+func (d imageData) Page(pageIndex int) ([]byte, bool) {
+	offset := pageIndex * d.pageSize
+	if offset >= len(d.image) {
+		return []byte{}, true
+	}
+
+	length := d.pageLength(pageIndex)
+	if offset+length > len(d.image) {
+		length = len(d.image) - offset
+	}
+
+	return d.image[offset : offset+length], pageIndex == d.PageCount()-1
+}
+
+func (d imageData) pageLength(pageIndex int) int {
+	remaining := len(d.image) - (pageIndex * d.pageSize)
+	if remaining > d.pageSize {
+		return d.pageSize
+	}
+	if remaining > 0 {
+		return remaining
+	}
+	return 0
+}
+
+// PageCount returns the total number of pages.
+func (d imageData) PageCount() int {
+	count := len(d.image) / d.pageSize
+	if len(d.image)%d.pageSize != 0 {
+		return count + 1
+	}
+	return count
+}
+
+// Length of the raw image data in bytes.
+func (d imageData) Length() int {
+	return len(d.image)
 }
